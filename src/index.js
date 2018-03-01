ef9e6391205dc7ea189313da248e003c0f57a348
falafel-v-postel <zztopgrodno@gmail.com>
2018-03-01 17:18:53 +0300

    brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var result = str;
+  for (var j = 0; j < (str.length/2); j++) {
+      for (var i = 0; i < bracketsConfig.length; i++) {
+          var brackets = bracketsConfig[i].join('');
+          result = result.replace(brackets, '');
+      }
+  }
+
+  return result === '';
 }
